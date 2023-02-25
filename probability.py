3a95d2bc7e96fcea6038a9400ebbcb3c34266434
AdamEshem <adameshemwork@gmail.com>
2023-02-25 18:15:54 -0500

    Finished collect_game_names, updated collect_page_values

--- probability.py
+++ probability.py
@@ -2,8 +2,8 @@ import requests
 import bs4
 from bs4 import BeautifulSoup
 
-def collect_page_values(cost, name):
-    page = requests.get('https://www.ohiolottery.com/Games/ScratchOffs/$' + cost + '-Games/' + name)
+def collect_page_values(name):
+    page = requests.get('https://www.ohiolottery.com/' + name)
 
     soup = BeautifulSoup(page.content, 'html5lib')
 
@@ -12,4 +12,25 @@ def collect_page_values(cost, name):
     for row in table.findAll('td', recursive=True):
         if row.text != '\xa0':
             values.append(row.text.strip('$ '))
+        
+    return values
 
+def collect_game_names():
+    page = requests.get('https://www.ohiolottery.com/Games/InstantGames')
+    name_soup = BeautifulSoup(page.content, 'html5lib')
+    
+    names = []
+    table = name_soup.findAll('div', attrs = {'class': 'list_wrap'}, recursive=True)
+    for i in table:
+        for ele in i.findAll('a', recursive=True):
+            names.append(ele['href'])
+    
+    return names
+
+names = collect_game_names()
+print(names)
+dict = {}
+for i in names:
+    values = collect_page_values(i)
+    dict[i] = values
+    print(dict)
