3c001411da0b13c4594395e2136f4138f1ee8fce
AdamEshem <adameshemwork@gmail.com>
2023-02-25 20:22:33 -0500

    Finished avg_value(). Not pretty but works

--- probability.py
+++ probability.py
@@ -32,12 +32,34 @@ def extract_cost(name):
     cost = re.findall(r'\d+', name[19: 23])
     return cost[0]
 
+def extract_name(name):
+    name = re.findall(r'-Games/.+', name)
+    return name[0][7:]
+
+def avg_value(dict):
+    for i in dict:
+        total = 0
+        num = 0
+        for j in range(0, len(dict[i]), 2):
+            dict[i][j] = dict[i][j].replace(',', '')
+            dict[i][j+1] = dict[i][j+1].replace(',', '')
+            if dict[i][j].replace('.', '0').isdigit():
+                total += float(dict[i][j]) * float(dict[i][j+1])
+            else:
+                total += float(input('value of ' + dict[i][j])) * float(dict[i][j+1])
+            num += int(dict[i][j+1])
+        print(i)
+        print(' Average: ')
+        print(total / num)
+        print('\n')
+                
 names = collect_game_names()
-print(names)
 dict = {}
 for i in names:
     cost = extract_cost(i)
-    values = collect_page_values(i)
-    dict[cost] = values
+    if cost == '1':
+        name = extract_name(i)
+        values = collect_page_values(i)
+        dict[name] = values
 
-
+avg_value(dict)
